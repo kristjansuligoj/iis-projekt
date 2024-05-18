3bb3ef3beac210476dd24910694fdfe50abd164f
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-18 16:33:47 +0200

    Created helping Spotify classes SpotifyClient and SpotifyTokenManager, that handle requests and token management

---
+++ src/classes/spotify/spotify_client.py
@@ -0,0 +1,64 @@
+from src.classes.spotify.spotify_token_manager import SpotifyTokenManager
+from src.classes.requests.requests_client import RequestsClient
+import os
+import base64
+
+
+class SpotifyClient:
+    def __init__(self):
+        self.spotify_authorization_url = os.getenv("SPOTIFY_AUTHORIZATION_URL")
+        self.spotify_api_token_url = os.getenv("SPOTIFY_API_TOKEN_URL")
+        self.spotify_api_url = os.getenv("SPOTIFY_API_URL")
+        self.client_id = os.getenv("SPOTIFY_CLIENT_ID")
+        self.client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+        client_credentials = f'{self.client_id}:{self.client_secret}'
+        self.client_credentials_base64 = base64.b64encode(client_credentials.encode()).decode()
+
+        self.token_manager = SpotifyTokenManager()
+        self.requests_client = RequestsClient()
+
+    # Sends an authenticated request to the spotify API.
+    # The request is attempted using an access token, and if it is expired,
+    # it uses a refresh token, to get a new access token.
+    def send_authenticated_request(self, method, url, headers=None, data=None):
+        access_token = self.token_manager.access_token['token']
+        response = self.requests_client.send_request(
+            method=method,
+            url=url,
+            headers={
+                'Authorization': f'Bearer {access_token}'
+            },
+            data=data
+        )
+
+        if 'error' in response:
+            print("Attempting to refresh access token and retrying the request")
+            self.token_manager.get_new_access_token_with_refresh_token()
+
+            # Retry the request with the new access token
+            access_token = self.token_manager.access_token['token']
+            response = self.requests_client.send_request(
+                method=method,
+                url=url,
+                headers={
+                    **headers,
+                    'Authorization': f'Bearer {access_token}'
+                },
+                data=data
+            )
+
+        return response
+
+    # Get recently played tracks of the authenticated user
+    def get_recently_played_tracks(self, limit=50):
+        return self.send_authenticated_request(
+            method="GET",
+            url=f"{self.spotify_api_url}/me/player/recently-played?limit={limit}",
+        )
+
+    # Get audio features of the provided tracks
+    def get_track_audio_features(self, track_ids):
+        return self.send_authenticated_request(
+            method="GET",
+            url=f"{self.spotify_api_url}/audio-features/?ids={track_ids}",
+        )
---
+++ src/classes/spotify/spotify_token_manager.py
@@ -0,0 +1,68 @@
+from src.classes.requests.requests_client import RequestsClient
+from src.database.connector import insert_token
+import os
+import base64
+from src.database.connector import fetch_latest_tokens
+
+
+class SpotifyTokenManager:
+    def __init__(self):
+        self.spotify_api_token_url = os.getenv("SPOTIFY_API_TOKEN_URL")
+        self.client_id = os.getenv("SPOTIFY_CLIENT_ID")
+        self.client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+        self.access_token, self.refresh_token = fetch_latest_tokens()
+
+        client_credentials = f'{self.client_id}:{self.client_secret}'
+        self.client_credentials_base64 = base64.b64encode(client_credentials.encode()).decode()
+        self.requests_client = RequestsClient()
+
+    # Uses the Authorization Code, to generate access and refresh tokens, then saves them to the database
+    # https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+    def get_tokens(self, code):
+        response = self.requests_client.send_request(
+            method="POST",
+            url=self.spotify_api_token_url,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': f'Basic {self.client_credentials_base64}'
+            },
+            data={
+                'grant_type': 'authorization_code',
+                'code': code,
+                'redirect_uri': "http://localhost:8080"
+            }
+        )
+
+        access_token = response['access_token']
+        refresh_token = response['refresh_token']
+
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+
+        # Insert both tokens to the database
+        insert_token('tokens', 'access', access_token)
+        insert_token('tokens', 'refresh', refresh_token)
+        pass
+
+    # Uses a refresh token to generate a new access token
+    def get_new_access_token_with_refresh_token(self):
+        response = self.requests_client.send_request(
+            method="POST",
+            url=self.spotify_api_token_url,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': f'Basic {self.client_credentials_base64}'
+            },
+            data={
+                'grant_type': 'refresh_token',
+                'refresh_token': self.refresh_token['token'],
+            }
+        )
+
+        access_token = response['access_token']
+
+        self.access_token = access_token
+
+        # Insert new access token to the database
+        insert_token('tokens', 'access', access_token)
+        pass
