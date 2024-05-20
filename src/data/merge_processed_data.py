e2f25b4aab5038f5f16cdd1b1df87a62137c15e3
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-20 20:45:22 +0200

    Updated 'merge_processed_data.py'

--- src/data/merge_processed_data.py
+++ src/data/merge_processed_data.py
@@ -29,7 +29,7 @@ def main():
 
         # Desired column order
         desired_order = [
-            'listened_at', 'artist', 'name', 'genre', 'length', 'explicit', 'popularity', 'danceability', 'energy', 'key',
+            'date', 'artist', 'name', 'genre', 'length', 'explicit', 'popularity', 'danceability', 'energy', 'key',
             'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence',
             'tempo', 'temperature', 'relative_humidity', 'dew_point', 'apparent_temperature',
             'precipitation_probability', 'rain', 'surface_pressure'
