2ac762f98abbf1a65d6a6f92cac652fbf4d81df0
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-26 10:46:49 +0200

    Updated 'merge_processed_data' and 'preprocess_weather_data' so tracks that have missing weather values, get filled correctly

--- src/data/merge_processed_data.py
+++ src/data/merge_processed_data.py
@@ -5,6 +5,41 @@ import pandas as pd
 import os
 
 
+# If a song is listened to at 11 am, the next time the GitHub Actions is ran could be the next day, so weather data
+# is missing, and validation fails, until you manually fix the data. This prevents that with checking all weather data
+# in the past and maps the missing data
+def check_if_weather_data_exists(df):
+    weather_columns = [
+        'temperature', 'relative_humidity', 'dew_point', 'apparent_temperature', 'precipitation_probability', 'rain',
+        'surface_pressure'
+    ]
+
+    # Get the indices of rows with missing values in any of the specified columns
+    missing_indices = df[weather_columns].isna().any(axis=1).to_numpy().nonzero()[0]
+
+    # Check if any of the specified columns contain NaN values
+    if len(missing_indices) > 0:
+        # We need all weather data, so we can fill the missing values
+        all_file_path = os.path.join(RAW_DATA_DIR, 'weather', 'all_weather.csv')
+        df_all_weather = pd.read_csv(all_file_path)
+
+        # Convert 'date' columns to datetime in both DataFrames for proper merging
+        df['date'] = pd.to_datetime(df['date'])
+        df_all_weather['date'] = pd.to_datetime(df_all_weather['date'])
+
+        # Fill missing values in specified columns with data from df_all_weather
+        for index in missing_indices:
+            # Find the row that is the closest to the track date (Weather is 1:00:00, track can be 1:23:03)
+            date = df.loc[index, 'date']
+            nearest_row = df_all_weather.iloc[(df_all_weather['date'] - date).abs().argsort()[0]]
+
+            # Fill the values from the nearest row
+            for columns in weather_columns:
+                df.loc[index, columns] = nearest_row[columns]
+
+    return df
+
+
 def main():
     try:
         df_spotify_file = os.path.join(RAW_DATA_DIR, "spotify", "preprocessed_spotify_data.csv")
@@ -40,12 +75,16 @@ def main():
 
         # Add the newly fetched data
         df = pd.concat([df, df_merged])
+        df.reset_index(drop=True, inplace=True)
 
         # Drop if any track is repeated. This can happen if user has not listened to any new songs since last fetch
         df = df.drop_duplicates(subset=['date'])
 
         # Sort by date to keep them in order
         df = df.sort_values(by='date')
+
+        # Check if any row has missing weather data, and fill it if it does
+        df = check_if_weather_data_exists(df)
 
         df_merged_file = os.path.join(PROCESSED_DATA_DIR, "processed_data.csv")
         DataManager.save_to_csv(df_merged_file, df, overwrite=True)
--- src/data/preprocess_weather_data.py
+++ src/data/preprocess_weather_data.py
@@ -2,16 +2,27 @@ from src.classes.data.DataManager import DataManager
 from definitions import RAW_DATA_DIR
 
 import pandas as pd
+from datetime import datetime
 import os
 
 
 def main():
     try:
+        # Read all weather data
+        all_file_path = os.path.join(RAW_DATA_DIR, 'weather', 'all_weather.csv')
+        all_df = pd.read_csv(all_file_path)
+        all_df['date'] = pd.to_datetime(all_df['date'])
+
         # Read today's weather
         today_weather_file = os.path.join(RAW_DATA_DIR, 'weather', 'raw_weather.csv')
         df_today = pd.read_csv(today_weather_file)
 
-        # Some processing . . .
+        today_date = datetime.now().date()
+
+        # If today weather data does not yet exist, save it
+        if not (today_date in all_df['date'].dt.date.unique()):
+            all_df_file = os.path.join(RAW_DATA_DIR, 'weather', 'all_weather.csv')
+            DataManager.save_to_csv(all_df_file, df_today)
 
         # Save to preprocessed file
         preprocessed_file_path = os.path.join(RAW_DATA_DIR, 'weather', 'preprocessed_weather_data.csv')
