8ed95b67f9c5ab87b1ac86c39d6fcbd799881f43
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-28 09:07:51 +0200

    Admin panel now shows metrics

--- src/client/src/app/components/pages/admin-panel-page/admin-panel-page.component.ts
+++ src/client/src/app/components/pages/admin-panel-page/admin-panel-page.component.ts
@@ -3,14 +3,18 @@ import {AuthService} from "../../../services/auth.service";
 import {Router} from "@angular/router";
 import {NgForOf} from "@angular/common";
 import {UnixTimestampToDatetimePipe} from "../../../pipes/unix-timestamp-to-datetime.pipe";
+import {ProductionMetricsComponent} from "../../production-metrics/production-metrics.component";
+import {MetricsOfTodayComponent} from "../../metrics-of-today/metrics-of-today.component";
 
 @Component({
   selector: 'app-admin-panel-page',
   standalone: true,
-    imports: [
-        NgForOf,
-        UnixTimestampToDatetimePipe
-    ],
+  imports: [
+    NgForOf,
+    UnixTimestampToDatetimePipe,
+    ProductionMetricsComponent,
+    MetricsOfTodayComponent
+  ],
   templateUrl: './admin-panel-page.component.html',
   styleUrl: './admin-panel-page.component.css'
 })
