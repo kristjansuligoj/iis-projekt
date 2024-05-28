ba42509a8f6ae0085b89bbfc7f5a81909aee569c
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-05-28 20:41:48 +0200

    Updated workflow

---
+++ src/client/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+
+  baseUrl: 'http://0.0.0.0:8080',
+  apiUrl: 'http://0.0.0.0:8080/api',
+};
---
+++ src/client/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+
+  baseUrl: 'http://0.0.0.0:8080',
+  apiUrl: 'http://0.0.0.0:8080/api',
+};
